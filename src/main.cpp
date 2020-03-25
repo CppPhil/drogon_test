02246396aa9b1f10c20a4ac43ef10d0b6bde366e
CppPhil <CppPhil@users.noreply.github.com>
2020-03-25 19:24:08 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@ using namespace drogon;
 
 int main() {
   app().addListener("127.0.0.1", 8848);
-  app().setDocumentRoot("./");
-  app().enableSession(60);
 
   auto ctrlPtr = std::make_shared<api::v1::User>();
 
--- src/user.cpp
+++ src/user.cpp
@@ -14,27 +14,30 @@ namespace v1 {
 void User::getInfo(const HttpRequestPtr& req,
                    std::function<void(const HttpResponsePtr&)>&& callback,
                    int userId) const {
+  LOG_DEBUG << "userId: " << userId;
+
   auto resp = HttpResponse::newHttpResponse();
   resp->setBody("user info for id: " + std::to_string(userId));
-  resp->setExpiredTime(0);
   callback(resp);
 }
 
 void User::getDetailInfo(const HttpRequestPtr& req,
                          std::function<void(const HttpResponsePtr&)>&& callback,
                          int userId) const {
+  LOG_DEBUG << "userId: " << userId;
+
   auto resp = HttpResponse::newHttpResponse();
   resp->setBody("detailed user info for id: " + std::to_string(userId));
-  resp->setExpiredTime(0);
   callback(resp);
 }
 
 void User::newUser(const HttpRequestPtr& req,
                    std::function<void(const HttpResponsePtr&)>&& callback,
                    std::string&& userName) {
+  LOG_DEBUG << "userId: " << userName;
+
   auto resp = HttpResponse::newHttpResponse();
   resp->setBody("newUser: " + userName);
-  resp->setExpiredTime(0);
   callback(resp);
 }
 } // namespace v1
