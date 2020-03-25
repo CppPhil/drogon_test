daaac96273482c60cf95e58709f7ffd26e35e700
CppPhil <CppPhil@users.noreply.github.com>
2020-03-25 18:50:19 +0100

    Update

--- src/user.cpp
+++ src/user.cpp
@@ -2,20 +2,40 @@
 
 namespace api {
 namespace v1 {
+/*
+ User {
+  int userId;
+  string name;
+  string info;
+  string detailed_info;
+ }
+*/
+
 void User::getInfo(const HttpRequestPtr& req,
                    std::function<void(const HttpResponsePtr&)>&& callback,
                    int userId) const {
+  auto resp = HttpResponse::newHttpResponse();
+  resp->setBody("user info for id: " + std::to_string(userId));
+  resp->setExpiredTime(0);
+  callback(resp);
 }
 
 void User::getDetailInfo(const HttpRequestPtr& req,
                          std::function<void(const HttpResponsePtr&)>&& callback,
                          int userId) const {
+  auto resp = HttpResponse::newHttpResponse();
+  resp->setBody("detailed user info for id: " + std::to_string(userId));
+  resp->setExpiredTime(0);
+  callback(resp);
 }
 
-void USer::newUser(const HttpRequestPtr& req,
+void User::newUser(const HttpRequestPtr& req,
                    std::function<void(const HttpResponsePtr&)>&& callback,
                    std::string&& userName) {
+  auto resp = HttpResponse::newHttpResponse();
+  resp->setBody("newUser: " + userName);
+  resp->setExpiredTime(0);
+  callback(resp);
 }
-
 } // namespace v1
 } // namespace api
